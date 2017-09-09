7706bdccbd7be76a6b7278f5775731da0425f052
rusualex <rusu_alex_geo@yahoo.com>
2017-09-09 12:34:02 +0300

    Delete and Update Student Methods in Repository

--- src/repository/RepoStudent.java
+++ src/repository/RepoStudent.java
@@ -4,6 +4,7 @@
 package repository;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import domain.Student;
@@ -13,15 +14,42 @@ import domain.Student;
  *
  */
 public class RepoStudent extends Student {
-	List<Student> studList = new ArrayList<Student>();
+	private List<Student> studList = new ArrayList<Student>();
+	private int i=0;
+	
+	
+	
+	
 	public void createStudent(int studMatrNr, String studName, int studSem, int studGrade) {
 		Student newStudent = new Student(studMatrNr, studName, studSem, studGrade);
 		studList.add(newStudent);
 	}
 	
+	public void deleteStudent(int studMatrNr){
+		for (Iterator<Student> iter = studList.listIterator(); iter.hasNext(); ) {
+		    Student removeCond = iter.next();
+		    if (removeCond.getStudMatrNr() == studMatrNr) {
+		        iter.remove();
+		    }
+		}
+	}
+	
+	public void updateStudent(int studMatrNr, String studName, int studSem, int studGrade){
+		for (Student stud : studList) {
+			if (stud.getStudMatrNr() == studMatrNr)
+			{
+				stud.setStudName(studName);
+				stud.setStudSem(studSem);
+				stud.setStudGrade(studGrade);
+			}
+		}
+		
+	}
+	
 	public void printStudentRepo(){
 		for (Student stud : studList) {
-			   System.out.println("Next item: " + stud.getStudPrinter());
+			   System.out.println("Studentul [" + i++ +"]: " + stud.getStudPrinter());
 			}
+		i=0;
 	}
 }
--- src/ui/FirstLab.java
+++ src/ui/FirstLab.java
@@ -15,6 +15,16 @@ public class FirstLab {
 		alex.setStudSem(1);
 		alex.setStudGrade(10);
 		System.out.println(alex.getStudPrinter());
+		
+		alex.createStudent(5, "Andrei", 1, 8); 
+		alex.createStudent(3, "Ana", 2, 6); 
+		alex.createStudent(2, "Rey", 1, 5); 
+		alex.createStudent(4, "Geo", 1, 3); 
+		alex.printStudentRepo();
+		alex.deleteStudent(4);
+		alex.printStudentRepo();
+		
+		
 	}
 
 }
