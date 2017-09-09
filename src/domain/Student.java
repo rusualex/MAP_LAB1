dc1a788b4b6af0bce0ca149cdc5b20908357de7c
rusualex <rusu_alex_geo@yahoo.com>
2017-09-09 10:55:06 +0300

    Project setup

---
+++ src/domain/Student.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package domain;
+
+/**
+ * @author Alex
+ *
+ */
+public class Student {
+	private int studMatrNr;
+	private String studName;
+	private int studSem;
+	private int studGrade;
+	/**
+	 * @return the studMatrNr
+	 */
+	public int getStudMatrNr() {
+		return studMatrNr;
+	}
+	/**
+	 * @param studMatrNr the studMatrNr to set
+	 */
+	public void setStudMatrNr(int studMatrNr) {
+		this.studMatrNr = studMatrNr;
+	}
+	/**
+	 * @return the studName
+	 */
+	public String getStudName() {
+		return studName;
+	}
+	/**
+	 * @param studName the studName to set
+	 */
+	public void setStudName(String studName) {
+		this.studName = studName;
+	}
+	/**
+	 * @return the studSem
+	 */
+	public int getStudSem() {
+		return studSem;
+	}
+	/**
+	 * @param studSem the studSem to set
+	 */
+	public void setStudSem(int studSem) {
+		this.studSem = studSem;
+	}
+	/**
+	 * @return the studGrade
+	 */
+	public int getStudGrade() {
+		return studGrade;
+	}
+	/**
+	 * @param studGrade the studGrade to set
+	 */
+	public void setStudGrade(int studGrade) {
+		this.studGrade = studGrade;
+	}
+}
